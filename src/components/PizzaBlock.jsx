3b05a3d8bf1a360fc19c4234feb1ec4bad4a5bba
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-04-18 15:09:05 +0300

    Added functionality for displaying the active selection of pizza type and size.

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,18 +1,37 @@
-function PizzaBlock({ imageUrl, title, price }) {
-  console.log(imageUrl);
+import React from "react";
+
+function PizzaBlock({ imageUrl, title, types, sizes, price }) {
+  const [activePizzaTypeIndex, setActivePizzaTypeIndex] = React.useState(0);
+  const [activePizzaSizeIndex, setActivePizzaSizeIndex] = React.useState(0);
+
+  const pizzaTypes = ["тонке", "традиційне"];
+
   return (
     <li className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li
+              key={type}
+              onClick={() => setActivePizzaTypeIndex(type)}
+              className={activePizzaTypeIndex === type ? "active" : ""}
+            >
+              {pizzaTypes[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, idx) => (
+            <li
+              key={idx}
+              onClick={() => setActivePizzaSizeIndex(idx)}
+              className={activePizzaSizeIndex === idx ? "active" : ""}
+            >
+              {size}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
