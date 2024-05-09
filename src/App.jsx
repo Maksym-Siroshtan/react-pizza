a97ff6e71c394dada71b3517c1e9589de0e12a38
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-05-09 19:56:31 +0300

    Added Redux functionality as an example counter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlice";
+
 import Header from "./components/Header";
 
 import Home from "./pages/Home";
@@ -9,16 +12,34 @@ import NotFound from "./pages/NotFound";
 import "./scss/app.scss";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="wrapper">
-      <Header />
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      {/* <Header />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </div>
+      </div> */}
     </div>
   );
 }
