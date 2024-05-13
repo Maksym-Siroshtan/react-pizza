47f305ad55f14993a4580fe97862e7459b544707
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-05-13 17:20:18 +0300

    Added focus on input when clearing.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,27 @@
+import React from "react";
+
 import searchLogo from "../assets/img/search.svg";
-import close from "../assets/img/close.svg";
+import clear from "../assets/img/close.svg";
 
 function Search({ search, setSearch }) {
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearch("");
+    inputRef.current.focus();
+  };
+
   return (
     <div className="content__search">
       <img src={searchLogo} alt="Search" className="search" />
       <input
+        ref={inputRef}
         onChange={() => setSearch(event.target.value)}
         value={search}
         type="text"
         placeholder="Пошук..."
       />
-      <img
-        onClick={() => setSearch("")}
-        src={close}
-        alt="Close"
-        className="close"
-      />
+      <img onClick={onClickClear} src={clear} alt="Clear" className="clear" />
     </div>
   );
 }
