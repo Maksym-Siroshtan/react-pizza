33c5cab95d5ac32b5cc6049fe9582f52875b713e
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-05-20 16:18:56 +0300

    Added CartEmpty component.

---
+++ src/components/CartEmpty.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+import cartEmpty from "../assets/img/empty-cart.png";
+
+function CartEmpty() {
+  return (
+    <div className="cart cart--empty">
+      <h2>
+        Кошик порожній <span>😕</span>
+      </h2>
+      <p>
+        Найімовірніше, Ви ще не замовляли піцу.
+        <br />
+        Для замовлення перейдіть на головну сторінку.
+      </p>
+      <img src={cartEmpty} alt="Empty cart" />
+      <Link to="/" className="button button--black">
+        <span>Повернутися назад</span>
+      </Link>
+    </div>
+  );
+}
+
+export default CartEmpty;
