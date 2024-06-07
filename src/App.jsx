bdf1e4321ca2e175e778e8ea8152cd214dbd411a
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-06-07 11:36:14 +0300

    The MainLayout has been created. Added dynamic routes using the Outlet hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import Header from "./components/Header";
-
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import SeparatePizza from "./components/SeparatePizza/SeparatePizza";
@@ -11,17 +10,14 @@ import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<SeparatePizza />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<SeparatePizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
